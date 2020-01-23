dde9a1312a770132a14fafffec5280bf82bdf38b
Yahya  Gok <ygok17@hotmail.com>
2020-01-23 02:22:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,7 @@ def monopoly_with_second_tier
   
   	{ :railroads => { 
   	  :pieces => 4
+  	  
 	  
 	}
 	}
@@ -29,7 +30,7 @@ def monopoly_with_third_tier
   # complexity.
     	{ :railroads => { 
   	  :pieces => 4
-	  
+	    :rent_in_dollars =>
 	}
 	}
   
